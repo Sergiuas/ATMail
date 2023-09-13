194720b552cfda2acafb458de6e6095877101f1b
Sergiu Amzuloiu <48670769+Sergiuas@users.noreply.github.com>
2023-09-13 17:56:51 +0300

    Delete 3_Solution/atmailSolution directory

--- 3_Solution/atmailSolution/AppCommon/app_net.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "app_common.h"
-#include "app_tsqueue.h"
-#include "app_message.h"
-#include "app_iclient.h"
-#include "app_iserver.h"
-#include "app_connection.h"
-#include "app_messagetype.h"
